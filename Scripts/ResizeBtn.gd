41665bcd991d68d03d7db7c57912b871b5213627
Krystof Klestil <krystofklestil@live.com>
2021-06-24 13:52:56 +0200

    ADD: Ability to resize the main app window.

---
+++ Scripts/ResizeBtn.gd
@@ -0,0 +1,26 @@
+extends TextureButton
+
+var MIN_WIN_SIZE : Vector2 = Vector2(1100, 650)
+
+var ms_speed : Vector2
+
+
+func _ready() -> void:
+	set_process_input(false)
+	
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		ms_speed = event.relative
+		OS.window_size += event.relative
+		OS.window_size.x = clamp(OS.window_size.x, MIN_WIN_SIZE.x, 10000)
+		OS.window_size.y = clamp(OS.window_size.y, MIN_WIN_SIZE.y, 10000)
+	if event is InputEventMouseButton and !event.pressed:
+		set_process_input(false)
+		Input.set_mouse_mode(0)
+		Input.warp_mouse_position(OS.window_size - Vector2(10,14))
+	
+	
+func _on_ResizeBtn_pressed() -> void:
+	set_process_input(true)
+	Input.set_mouse_mode(2)
