5fa1e9e5fac76ebed4f282f29d2a1835d8621a6b
Krystof Klestil <krystofklestil@live.com>
2021-11-04 08:59:58 +0100

    REMOVE: Reminders

--- Scripts/Views.gd
+++ Scripts/Views.gd
@@ -12,6 +12,7 @@ func hide_all() -> void:
 	for i in get_children():
 		i.hide()
 
+
 func start_custom_time_track(_name) -> void:
 	emit_signal("manual_view_toggle", 2)
 	$TimeTracking.start_time_tracking_custom(_name)
