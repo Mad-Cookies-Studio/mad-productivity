bd4a9e92a4457c77e9aac5c49c9c5881d3569dd9
Krystof Klestil <krystofklestil@live.com>
2021-11-04 22:58:52 +0100

    REMOVE: Reminders remnants

--- Assets/Resources/ReminderResource.gd
+++
@@ -1,9 +0,0 @@
-class_name ReminderResource
-extends Resource
-
-export(String) var title
-export(String) var text
-export(String) var link
-export(String) var saved_date
-export(String) var saved_time
-export(Dictionary) var reminders
