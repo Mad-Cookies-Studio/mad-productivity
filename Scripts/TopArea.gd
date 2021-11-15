a7662d29ff317f50a08dc5bf077af5801a278340
Krystof Klestil <krystofklestil@live.com>
2021-11-15 12:01:21 +0100

    CHANGE: Move signal calls to a function

--- Scripts/TopArea.gd
+++ Scripts/TopArea.gd
@@ -14,61 +14,62 @@ var minimized_pos : Vector2
 var offset : Vector2 
 
 func _ready() -> void:
-    # ....
-    connect("mouse_entered", self, "_on_mouse_entered")
-    connect("mouse_exited", self, "_on_mouse_exited")
-    # ... should be moved to a _connect_signals() function?
-    
-    Defaults.connect("view_changed", self, "on_view_changed")
-    Defaults.connect("update_view_info", self, "on_update_view_info")
-
-    var res = load(Defaults.TIMETRACKS_SAVE_PATH + Defaults.TIMETRACKS_SAVE_NAME)	# TODO: access this resource 
-        
+	connect_signals()
+	var res = load(Defaults.TIMETRACKS_SAVE_PATH + Defaults.TIMETRACKS_SAVE_NAME)	# TODO: access this resource 
+		
+		
+func connect_signals() -> void:
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+	Defaults.connect("view_changed", self, "on_view_changed")
+	Defaults.connect("update_view_info", self, "on_update_view_info")
+		
+		
 func _on_mouse_entered() -> void:
-    set_process_input(true)
+	set_process_input(true)
 
 func _on_mouse_exited() -> void:
-    set_process_input(false)
+	set_process_input(false)
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        initial_mouse_pos = get_global_mouse_position()
-        if event.pressed:
-            offset = get_global_mouse_position()
-        else:
-            offset = Vector2()
-    if event is InputEventMouseMotion and offset != Vector2():
-        if OS.window_maximized:
-            offset *=  Defaults.settings_res.minimized_window_size.x / OS.get_window_size().x
-            $Right/Maximize.pressed = false
-        OS.set_window_position(OS.get_window_position() + event.get_global_position() - offset)
+	if event is InputEventMouseButton:
+		initial_mouse_pos = get_global_mouse_position()
+		if event.pressed:
+			offset = get_global_mouse_position()
+		else:
+			offset = Vector2()
+	if event is InputEventMouseMotion and offset != Vector2():
+		if OS.window_maximized:
+			offset *=  Defaults.settings_res.minimized_window_size.x / OS.get_window_size().x
+			$Right/Maximize.pressed = false
+		OS.set_window_position(OS.get_window_position() + event.get_global_position() - offset)
 
 
 func _on_Minimuze_pressed() -> void:
-    OS.window_minimized = true
+	OS.window_minimized = true
 
 
 func _on_Exit_pressed() -> void:
-    Defaults.quit()
+	Defaults.quit()
 
 
 func _on_Maximize_toggled(button_pressed: bool) -> void:
-    if button_pressed:
-        Defaults.settings_res.minimized_window_size = OS.window_size
-        Defaults.settings_res.minimized_window_position = OS.window_position
-        Defaults.settings_res.window_maximized = true
-        Defaults.save_settings_resource()
-        OS.window_maximized = button_pressed
-    else:
-        OS.window_maximized = false
-        Defaults.settings_res.window_maximized = false
-        OS.window_size = Defaults.settings_res.minimized_window_size
-        OS.window_position = Defaults.settings_res.minimized_window_position
-        Defaults.save_settings_resource()
-                
+	if button_pressed:
+		Defaults.settings_res.minimized_window_size = OS.window_size
+		Defaults.settings_res.minimized_window_position = OS.window_position
+		Defaults.settings_res.window_maximized = true
+		Defaults.save_settings_resource()
+		OS.window_maximized = button_pressed
+	else:
+		OS.window_maximized = false
+		Defaults.settings_res.window_maximized = false
+		OS.window_size = Defaults.settings_res.minimized_window_size
+		OS.window_position = Defaults.settings_res.minimized_window_position
+		Defaults.save_settings_resource()
+				
 
 func change_window_title(_name : String) -> void:
-    $Left/ViewLabel.text = _name
+	$Left/ViewLabel.text = _name
 #	$Tween.stop_all()
 #	$Tween.interpolate_property($Right/ViewLabel, "percent_visible", $Right/ViewLabel.percent_visible, 0.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.0)
 #	$Tween.interpolate_property($Right/ViewLabel, "percent_visible", 0.0, 1.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.5)
@@ -79,36 +80,36 @@ func change_window_title(_name : String) -> void:
 
 # IMPORTANT: This func sets up the top area according to how to the new view
 func on_view_changed(_name : String, _button : bool, _input_field : bool) -> void:
-    change_window_title(_name)
-    if _name == "Time tracking":
-        $Left/NewBtn.hide()
-        $Left/LineEdit.hide()
-    else:
-        $Left/LineEdit.visible = _input_field
-        $Left/NewBtn.visible = _button
+	change_window_title(_name)
+	if _name == "Time tracking":
+		$Left/NewBtn.hide()
+		$Left/LineEdit.hide()
+	else:
+		$Left/LineEdit.visible = _input_field
+		$Left/NewBtn.visible = _button
 
 
 func _on_Button_pressed() -> void:
-    if Defaults.active_view_pointer and Defaults.active_view_pointer.has_method("on_new_top_bar_button"):
-        var message : Dictionary = {}
-        if $Left/LineEdit.text != "":
-            message.text = $Left/LineEdit.text
-            $Left/LineEdit.clear()
-        Defaults.active_view_pointer.on_new_top_bar_button(message)
+	if Defaults.active_view_pointer and Defaults.active_view_pointer.has_method("on_new_top_bar_button"):
+		var message : Dictionary = {}
+		if $Left/LineEdit.text != "":
+			message.text = $Left/LineEdit.text
+			$Left/LineEdit.clear()
+		Defaults.active_view_pointer.on_new_top_bar_button(message)
 
 
 func _on_Shortcuts_shortcut_use() -> void:
-    _on_Button_pressed()
+	_on_Button_pressed()
 
 
 func _on_Shortcuts_shortcut_focus() -> void:
-    if $Left/LineEdit.visible:
-        $Left/LineEdit.grab_focus()
+	if $Left/LineEdit.visible:
+		$Left/LineEdit.grab_focus()
 
 
 func on_update_view_info(text : String) -> void:
-    if text:
-        $Right/ViewInfoPanel.show()
-        $Right/ViewInfoPanel/ViewInfoLabel.text = text
-    else:
-        $Right/ViewInfoPanel.hide()
+	if text:
+		$Right/ViewInfoPanel.show()
+		$Right/ViewInfoPanel/ViewInfoLabel.text = text
+	else:
+		$Right/ViewInfoPanel.hide()
